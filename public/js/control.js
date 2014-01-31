40aeccecd8aa9bbff7327cb88fb59f6337802257
Seong Cho <axelcho@gmail.com>
2014-01-30 20:20:54 -0500

    corrected element

--- public/js/control.js
+++ public/js/control.js
@@ -34,7 +34,7 @@ function requestFullScreen(el) {
   var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
 
   if (requestMethod) { // Native full screen.
-    requestMethod.call(element);
+    requestMethod.call(el);
   } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
     var wscript = new ActiveXObject("WScript.Shell");
     if (wscript !== null) {
