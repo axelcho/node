4d2cffffb0701a085bfa138a12d390e1b2e1f999
Seong Cho <axelcho@gmail.com>
2014-01-30 19:35:37 -0500

    removed chatting window

--- public/js/control.js
+++ public/js/control.js
@@ -7,58 +7,20 @@ window.onload = function() {
     var content = document.getElementById("content");
     var name = document.getElementById("name");		
  
-    socket.on('message', function (data) {
-        if(data.message) {		
-            messages.push(data);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += '<b>' + (messages[i].username ? messages[i].username  + messages[i].entrydate : 'System') + ': </b>' ;
-                html += messages[i].message + '<br />';
-            }
-            content.innerHTML = html;
-			content.scrollTop = content.scrollHeight;
-			
-        } else {
+    socket.on('message', function (data) {		
+		if (data.choice) {
+			$('#main').load('http://localhost/html/' + data.choice + '.html');		
+		}
+		
+		else {
             console.log("There is a problem:", data);
         }
-		
-		if (data.choice) {
-		$('#main').load('http://localhost/html/' + data.choice + '.html');
-		
-		}
 		
 		
     });
  
 	//sendmessage event
     sendButton.onclick = sendMessage = function() {
-        if(name.value == "") {
-            alert("Please type your name!");
-        } else {
-		
-		//add current date time
-			var currentdate = new Date(); 
-			var datetime = "("
-				+ (currentdate.getMonth()+1)  + "/"
-                + currentdate.getDate() + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds() + ") ";
-		
-            var text = field.value;
-
-            socket.emit('send', { message: text, username: name.value, entrydate: datetime, choice: station.value });
-			field.value = "";
-        }
+        socket.emit('send', {choice: station.value });        
     };
-	
-	//send message when enter key is pressed at the message
-	$(document).ready(function() {
-    $("#field").keyup(function(e) {
-        if(e.keyCode == 13) {
-            sendMessage();
-        }
-    });
-	}); 
 }
