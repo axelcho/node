5dc3393ed1577526d87a5c7cd373a2df78e7bb5e
Seong Cho <axelcho@gmail.com>
2014-01-30 20:29:13 -0500

    full screen request

--- public/js/control.js
+++ public/js/control.js
@@ -1,9 +1,22 @@
-window.onload = function() {
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+window.onload = function() { 
 
     var elem = document.body; // Make the body go full screen.
-	requestFullScreen.call(elem);
- 
-    var messages = [];
+	requestFullScreen(elem);
+    
     var socket = io.connect('http://localhost');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
